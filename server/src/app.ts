20e7560aec9a294479ba43b706a6016d750dafc3
Shubham <shubhampatilsd@gmail.com>
2022-09-08 12:49:21 -0700

    connection bug

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,37 @@
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
-import { onConnection, runSocket } from "./routes/onConnections";
+import cors from "cors";
+import path from "path";
+import { runSocket } from "./routes/onConnections";
 
 const app = express();
-const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send("Hi there!");
+  res.json({
+    success: true,
+    message: `API`,
+  });
 });
 
+app.get("/socket.io/socket.io.js", (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "..",
+      "/node_modules/socket.io/client-dist/socket.io.js"
+    )
+  );
+});
+
+export const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5000;
 runSocket();
 
 server.listen(PORT, () => {
--- server/src/routes/onConnections.ts
+++ server/src/routes/onConnections.ts
@@ -1,11 +1,20 @@
 import { Server, Socket } from "socket.io";
 import { sendFile } from "./sendFile";
 import { generateName } from "../../util/generateName";
-const io = new Server();
+import { server } from "../app";
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"],
+  },
+});
+
+export const connections = new Map();
 
 export const onConnection = (socket: Socket) => {
   const name = generateName();
-  io.to(socket.id).emit("assignName");
+  io.to(socket.id).emit("assignName", name);
+  connections.set(socket.id, name);
   socket.on("sendFile", sendFile);
 };
 
